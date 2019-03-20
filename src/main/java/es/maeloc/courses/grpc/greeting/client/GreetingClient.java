111508835ff94411b73f0bf57962312b3607ab3a
maeloc <anxogomez@gmail.com>
2019-03-20 18:34:28 +0100

    after Unary Greeting API

---
+++ src/main/java/es/maeloc/courses/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,51 @@
+package es.maeloc.courses.grpc.greeting.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051 )
+                .usePlaintext()
+                .build();
+
+        System.out.println("Creating stub");
+
+        // sync dummy client
+        //DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+        // async dummy client
+        // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
+
+        // create a greet service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        // create a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Pepe")
+                .setLastName("Solla")
+                .build();
+
+        // create a protocol buffer greet request
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a greet Response (using protocol buffers)
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        System.out.println(greetResponse.getResult());
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+
+}
